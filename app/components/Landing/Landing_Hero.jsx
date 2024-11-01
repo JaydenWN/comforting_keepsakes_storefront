39746265f337c6aaf5531f771243d352d48f6f83
JaydenWN <123134488+JaydenWN@users.noreply.github.com>
2024-11-01 14:32:55 +1100

    responsive landing hero

--- app/components/Landing/Landing_Hero.jsx
+++ app/components/Landing/Landing_Hero.jsx
@@ -3,21 +3,26 @@ import { NavLink } from "@remix-run/react";
 export default function Landing_Hero() {
 	return (
 		<section className={`${styles["landing-hero"]} gutter-p section-p`}>
-			<img src="/images/keepsake_bear_landing_image.jpg" alt="hero" />
+			<div className={styles["landing_img"]}>
+				<img src="/images/keepsake_bear_landing_image.jpg" alt="hero" />
+			</div>
 
-			<h1>
-				<span>Handcrafted Keepsakes</span> That Cherish Every Memory.
-			</h1>
-			<NavLink className="primary_button" to={"/store"}>
-				Shop Now
-			</NavLink>
-
-			<p>
-				We create memory bears, beautifully handcrafted from your loved one’s
-				clothing. These bears serve as a tangible reminder of cherished
-				memories, offering comfort during times of loss and becoming a lasting
-				keepsake for you and your family.
-			</p>
+			<div className={styles["landing_content"]}>
+				<h1>
+					<span>Handcrafted Keepsakes</span> That Cherish Every Memory.
+				</h1>
+				<NavLink className="primary_button" to={"/store"}>
+					Shop Now
+				</NavLink>
+			</div>
+			<div className={styles["hero_text"]}>
+				<p>
+					We create memory bears, beautifully handcrafted from your loved one’s
+					clothing. These bears serve as a tangible reminder of cherished
+					memories, offering comfort during times of loss and becoming a lasting
+					keepsake for you and your family.
+				</p>
+			</div>
 		</section>
 	);
 }
